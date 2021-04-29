b5bf27ebfb87462476facb832a6f543b99cc5d82
Mike <15151851516@163.com>
2021-04-29 16:53:05 +0800

    改名 xxx

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -13,7 +13,7 @@ import {inject, Ref} from 'vue';
 
 export default {
   setup() {
-    const asideVisible = inject<Ref<boolean>>('xxx');
+    const asideVisible = inject<Ref<boolean>>('asideVisible');
     console.log(asideVisible.value);
     const toggleAside = () => {
       asideVisible.value = !asideVisible.value;
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -31,7 +31,7 @@ import {inject, Ref} from 'vue';
 export default {
   components: {TopNav},
   setup() {
-    const asideVisible = inject<Ref<boolean>>('xxx');
+    const asideVisible = inject<Ref<boolean>>('asideVisible');
     return {asideVisible};
   }
 };
