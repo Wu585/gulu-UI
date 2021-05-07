25876059777566c943bfe4965f3cab432712a0ac
Mike <15151851516@163.com>
2021-05-07 13:42:51 +0800

    openlog 一句话打开 dialog

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -11,12 +11,15 @@
       <strong>加粗的标题</strong>
     </template>
   </Dialog>
+  <h1>示例2</h1>
+  <Button @click="showDialog">show</Button>
 </template>
 
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue';
 import Button from '../lib/Button.vue';
 import {ref} from 'vue';
+import {openDialog} from '../lib/openDialog';
 
 export default {
   components: {
@@ -32,10 +35,12 @@ export default {
       return false;
     };
     const f2 = () => {
-
+    };
+    const showDialog = () => {
+      openDialog({title: '标题', content: '你好'});
     };
     return {
-      x, toggle, f1, f2
+      x, toggle, f1, f2, showDialog
     };
   }
 };
