f3704e6d5393d787f34f009af0a38df23f66c8df
Mike <15151851516@163.com>
2021-05-21 10:51:58 +0800

    使用 watchEffect 优化代码

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -20,7 +20,7 @@
 
 <script lang="ts">
 import Tab from './Tab.vue';
-import {onMounted, onUpdated, ref} from 'vue';
+import {onMounted, onUpdated, ref, watchEffect} from 'vue';
 
 export default {
   props: {
@@ -32,16 +32,17 @@ export default {
     const selectedItem = ref<HTMLDivElement>(null);
     const indicator = ref<HTMLDivElement>(null);
     const container = ref<HTMLDivElement>(null);
-    const x = () => {
-      const {width} = selectedItem.value.getBoundingClientRect();
-      indicator.value.style.width = width + 'px';
-      const {left: left1} = container.value.getBoundingClientRect();
-      const {left: left2} = selectedItem.value.getBoundingClientRect();
-      const left = left2 - left1;
-      indicator.value.style.left = left + 'px';
-    };
-    onMounted(x);
-    onUpdated(x);
+    onMounted(() => {
+          watchEffect(() => {
+            const {width} = selectedItem.value.getBoundingClientRect();
+            indicator.value.style.width = width + 'px';
+            const {left: left1} = container.value.getBoundingClientRect();
+            const {left: left2} = selectedItem.value.getBoundingClientRect();
+            const left = left2 - left1;
+            indicator.value.style.left = left + 'px';
+          });
+        }
+    );
     const select = (title: string) => {
       context.emit('update:selected', title);
     };
